239645dfaec5ca1c8ec2047b295d1ad4a37a27d6
Vitaly <onebeattrue@gmail.com>
2023-07-18 23:37:36 +0400

    presentation using live data

--- app/src/main/java/edu/mirea/onebeattrue/purchaselist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/edu/mirea/onebeattrue/purchaselist/data/ShopListRepositoryImpl.kt
@@ -1,24 +1,37 @@
 package edu.mirea.onebeattrue.purchaselist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import edu.mirea.onebeattrue.purchaselist.domain.ShopItem
 import edu.mirea.onebeattrue.purchaselist.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl : ShopListRepository {
 
+    private val shopListLiveData = MutableLiveData<List<ShopItem>>()
+
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -32,7 +45,11 @@ object ShopListRepositoryImpl : ShopListRepository {
             ?: throw RuntimeException("Element with id $shopItemId not found") // обработка исключения
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList() // для возврата копии
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLiveData
+    }
+
+    private fun updateList() {
+        shopListLiveData.value = shopList.toList()
     }
 }
