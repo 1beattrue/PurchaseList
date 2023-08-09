f2742c3c9b19a4a3741c76890cd1d41c07fc6c24
Vitaly <onebeattrue@gmail.com>
2023-08-09 20:09:56 +0300

    Перенесение логики работы Activity во Fragment При перевороте экрана случается краш

---
+++ app/src/main/java/edu/mirea/onebeattrue/purchaselist/presentation/ShopItemFragment.kt
@@ -0,0 +1,160 @@
+package edu.mirea.onebeattrue.purchaselist.presentation
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputLayout
+import edu.mirea.onebeattrue.purchaselist.R
+import edu.mirea.onebeattrue.purchaselist.domain.ShopItem
+
+class ShopItemFragment(
+    private val screenMode: String = MODE_UNKNOWN,
+    private val shopItemId: Int = ShopItem.UNDEFINED_ID
+) : Fragment() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
+    private lateinit var btnSave: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shop_item, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) { // метод, в котором безопаснее работать с view (вызывается, когда оно точно будет создано)
+        super.onViewCreated(view, savedInstanceState)
+
+        parseParams()
+
+        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
+
+        initViews(view)
+        addTextChangedListeners()
+        launchRightMode()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputName.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            tilName.error = message
+        }
+
+        viewModel.errorInputCount.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.error_input_count)
+            } else {
+                null
+            }
+            tilCount.error = message
+        }
+
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            activity?.onBackPressed() // аналогичный метод requireActivity() не может быть null (не совсем безопасно)
+        }
+    }
+
+    private fun addTextChangedListeners() {
+        etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+
+        etCount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                viewModel.resetErrorInputCount()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
+    }
+
+    private fun initViews(view: View) {
+        tilName = view.findViewById(R.id.til_name)
+        tilCount = view.findViewById(R.id.til_count)
+        etName = view.findViewById(R.id.etName)
+        etCount = view.findViewById(R.id.etCount)
+        btnSave = view.findViewById(R.id.btn_save)
+    }
+
+    private fun launchRightMode() {
+        when (screenMode) {
+            MODE_ADD -> launchAddMode()
+            MODE_EDIT -> launchEditMode()
+        }
+    }
+
+    private fun launchAddMode() {
+        btnSave.setOnClickListener {
+            viewModel.addShopItem(etName.text?.toString(), etCount.text?.toString())
+        }
+    }
+
+    private fun launchEditMode() {
+        viewModel.getShopItem(shopItemId)
+        viewModel.shopItem.observe(viewLifecycleOwner) {
+            etName.setText(it.name)
+            etCount.setText(it.count.toString())
+        }
+        btnSave.setOnClickListener {
+            viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
+        }
+    }
+
+    private fun parseParams() {
+        if (screenMode != MODE_EDIT && screenMode != MODE_ADD)
+            throw RuntimeException("Param screen mode is absent")
+
+        if (screenMode == MODE_EDIT && shopItemId == ShopItem.UNDEFINED_ID)
+            throw RuntimeException("Param shop item id is absent")
+    }
+
+    companion object {
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+
+        private const val MODE_UNKNOWN = ""
+
+        fun newInstanceAddItem(): Fragment {
+            return ShopItemFragment(MODE_ADD)
+        }
+
+        fun newInstanceEditItem(shopItemId: Int): Fragment {
+            return ShopItemFragment(MODE_EDIT, shopItemId)
+        }
+    }
+}
